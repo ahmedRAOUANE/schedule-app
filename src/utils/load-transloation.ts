f86e6392d9156c77350fd074709ae5d87e75df31
ahmedRAOUANE <ahmedraouane30@gmail.com>
2025-03-28 21:08:55 +0100

    fix load translations featch url issue

--- src/utils/load-transloation.ts
+++ src/utils/load-transloation.ts
@@ -1,7 +1,8 @@
 import { Language, Translation } from "./types/language";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const loadTranslation = async (lang: Language): Promise<Translation> => {
-    const res = await fetch(`http://localhost:3000/api/translations/${lang}`);
+    const res = await fetch(`${baseUrl}/api/translations/${lang}`);
     return await res.json();
 }
